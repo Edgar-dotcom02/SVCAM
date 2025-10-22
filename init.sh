0461f408af0ae5e9af25441c4db91fa6364b103e
Edgar-dotcom02 <114014464+Edgar-dotcom02@users.noreply.github.com>
2025-10-22 18:39:00 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo apt-get -y update --fix-missing
-sudo apt-get -y upgrade
-sudo apt-get install -y build-essential libtbb-dev
-sudo apt-get install -y libtool pkg-config autoconf automake uuid-dev
+if [[ $EUID -ne 0 ]]; then
+   echo "ОШИБКА: Запустите скрипт с sudo: sudo ./init.sh"
+   exit 1
+fi
+
+apt-get -y update --fix-missing
+apt-get -y upgrade
+apt-get install -y build-essential libtbb-dev pkg-config libtool autoconf automake uuid-dev
