c2559f5c8de8ea5b5c923298d53444f6a8210fbc
Edgar-dotcom02 <114014464+Edgar-dotcom02@users.noreply.github.com>
2025-10-23 09:46:37 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+usermod -a -G dialout pi
 apt-get -y update --fix-missing
 apt-get -y upgrade
 apt-get install -y build-essential libtbb-dev pkg-config libtool autoconf automake uuid-dev
