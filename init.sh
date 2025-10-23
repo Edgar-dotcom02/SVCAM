bade9ea6bccbff4e21b9fa19d8c362c3dde35e80
Edgar-dotcom02 <114014464+Edgar-dotcom02@users.noreply.github.com>
2025-10-23 09:49:44 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "=== Обновление пакет программ для SVCAM ==="
+
 if [[ $EUID -ne 0 ]]; then
    echo "ОШИБКА: Запустите скрипт с sudo: sudo ./init.sh"
    exit 1
@@ -9,3 +11,5 @@ usermod -a -G dialout pi
 apt-get -y update --fix-missing
 apt-get -y upgrade
 apt-get install -y build-essential libtbb-dev pkg-config libtool autoconf automake uuid-dev
+
+echo "=== Обновление ОКОНЧЕНО ==="
